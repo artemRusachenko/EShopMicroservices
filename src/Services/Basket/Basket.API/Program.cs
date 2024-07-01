addd4e0e48675e52d426c9caa951b9265c08de91
artemRusachenko <artemrusachenko@gmail.com>
2024-07-01 22:46:50 +0300

    Apply distributed caching with Redis

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,7 @@
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,12 +22,34 @@ builder.Services.AddMarten(opts =>
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+//The same way without using library
+/*builder.Services.AddScoped<IBasketRepository>(provider =>
+{
+    var basketRepository = provider.GetRequiredService<BasketRepository>();
+    return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+});*/
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapCarter();
 app.UseExceptionHandler(options =>{});
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
 app.Run();
